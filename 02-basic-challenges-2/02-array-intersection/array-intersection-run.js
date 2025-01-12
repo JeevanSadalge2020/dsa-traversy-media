e63826e25ebacbfefca40e82eb48163f0463a32f
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-01-12 12:39:13 +0530

    solve array-interaction

--- 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
@@ -1,5 +1,6 @@
-const arrayIntersection = require('./array-intersection');
+const arrayIntersection = require("./array-intersection");
 
-const result = arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+const result = arrayIntersection([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
+// const result = arrayIntersection([1, 2, 3, 4, 5, 3, 4, 5], [3, 4, 5]);
 
 console.log(result);
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,22 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let res = [];
+  let arr1_length = arr1.length;
+  let arr2_length = arr2.length;
+
+  if (arr1_length < arr2_length) {
+    for (let i = 0; i < arr1_length; i++) {
+      if (arr2.includes(arr1[i])) {
+        res.push(arr1[i]);
+      }
+    }
+  } else {
+    for (let i = 0; i < arr2_length; i++) {
+      if (arr1.includes(arr2[i])) {
+        res.push(arr2[i]);
+      }
+    }
+  }
+  return res;
+}
 
 module.exports = arrayIntersection;
