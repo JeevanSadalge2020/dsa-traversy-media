fa425fdfa31c32869c944c80d631f0f646a7f44f
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-01-15 17:16:55 +0530

    DSA : solve display-likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,20 @@
-function displayLikes() {}
+function displayLikes(names) {
+  let arrLength = names.length;
+  if (arrLength === 0) return "no one likes this";
+  else {
+    switch (arrLength) {
+      case 1:
+        return `${names[0]} likes this`;
+      case 2:
+        return `${names[0]} and ${names[1]} like this`;
+      case 3:
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      default:
+        return `${names[0]}, ${names[1]} and ${
+          names.length - 2
+        } others like this`;
+    }
+  }
+}
 
 module.exports = displayLikes;
