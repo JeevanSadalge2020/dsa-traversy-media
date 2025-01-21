53eeb07308c98e37a5c4c4f5e71bf04b4419a2e8
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-01-22 00:04:04 +0530

    DSA : solve - format phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  let a = arr.slice(0, 3);
+  let b = arr.slice(3, 6);
+  let c = arr.slice(6);
+  return `(${a.join("")}) ${b.join("")}-${c.join("")}`;
+}
 
 module.exports = formatPhoneNumber;
