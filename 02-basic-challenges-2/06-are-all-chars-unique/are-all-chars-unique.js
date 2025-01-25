88adfd74f2611a5ff61731eded4628d46098aa34
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-01-25 23:03:26 +0530

    DSA - Solve : are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,17 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let l = str.length;
+  if (l === 0) return true;
+  else {
+    let map = new Map();
+    for (let i = 0; i < l; i++) {
+      if (!map.has(str[i])) {
+        map.set(str[i], true);
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
 module.exports = areAllCharactersUnique;
