f3239307799995a630fe2a1f52eb4ecaf26f244f
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-02-01 17:11:18 +0530

    DSA: solve find-missing-number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
@@ -1,5 +1,6 @@
-const findMissingNumber = require('./find-missing-number');
+const findMissingNumber = require("./find-missing-number");
 
 const result = findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1]);
+const result1 = findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1, 9]);
 
-console.log(result);
+console.log(result1);
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,12 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+  let sum = (n * (n + 1)) / 2;
+  let arr_sum = arr.reduce((res, cur, arr) => cur + res);
+  console.log("sum", sum);
+  console.log("arr_sum", arr_sum);
+  if (!arr) return undefined;
+  if (n === 0) return 1;
+  return sum - arr_sum;
+}
 
 module.exports = findMissingNumber;
