54238fd0e586f1ed99126e26ce6218edcb44083d
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-02-12 23:40:24 +0530

    DSA: Solve - find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter-run.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter-run.js
@@ -1,5 +1,10 @@
-const findMissingLetter = require('./find-missing-letter');
+const findMissingLetter = require("./find-missing-letter");
 
-result = findMissingLetter(['a', 'b', 'c', 'd', 'f', 'g']);
+result = findMissingLetter(["a", "b", "c", "d", "f", "g"]);
 
 console.log(result);
+
+console.log(findMissingLetter(["a", "b", "d", "e"])); // Should return 'c'
+console.log(findMissingLetter(["O", "Q", "R", "S"])); // Should return 'P'
+console.log(findMissingLetter([])); // Should return ''
+console.log(findMissingLetter(["a", "b", "c"])); // Should return ''
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,15 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  if (arr.length === 0) return "";
+  else {
+    for (let i = 1; i < arr.length; i++) {
+      let cur = arr[i].charCodeAt(0);
+      let prev = arr[i - 1].charCodeAt(0);
+      if (cur - prev > 1) {
+        return String.fromCharCode(cur - 1);
+      }
+    }
+    return "";
+  }
+}
 
 module.exports = findMissingLetter;
