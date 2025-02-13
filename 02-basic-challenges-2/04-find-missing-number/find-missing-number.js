d326d53dd31aba9e56496dfd28c5913412081d75
Jeevan Sadalge <jeevanhrx@gmail.com>
2025-02-13 20:39:22 +0530

    DSA:find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -2,8 +2,6 @@ function findMissingNumber(arr) {
   let n = arr.length + 1;
   let sum = (n * (n + 1)) / 2;
   let arr_sum = arr.reduce((res, cur, arr) => cur + res);
-  console.log("sum", sum);
-  console.log("arr_sum", arr_sum);
   if (!arr) return undefined;
   if (n === 0) return 1;
   return sum - arr_sum;
